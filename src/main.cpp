36d4a720f0e26b67fa6a0b8536a244c99f646eab
Evan Hanson <hans2624@stthomas.edu>
2021-05-11 22:23:03 -0500

    Implemented code for limiting files displayed on a page to allow for paginating. Paginating will be implemented once clicking method is added. Icons have been added but are not yet differing based on what the file extension is.

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,20 @@
 
 typedef struct AppData {
     std::vector<std::string> files;
-    std::vector<std::string> file_sizes;
+    std::vector<int> file_sizes;
     TTF_Font *font;
-    SDL_Texture *penguin;
+    SDL_Texture *directory_icon;
+    SDL_Texture *image_icon;
+    SDL_Texture *right_arrow_icon;
+    SDL_Texture *left_arrow_icon;
     //SDL_Texture *phrase;
     std::vector<SDL_Texture*> files_textures;
     std::vector<SDL_Texture*> file_sizes_textures;
-    SDL_Rect penguin_rect;
     std::vector<SDL_Rect*> files_rect;
+    std::vector<SDL_Rect*> icon_rect;
+    SDL_Rect right_arrow_rect;
+    SDL_Rect left_arrow_rect;
+    int page = 0;
 } AppData;
 
 void initialize(SDL_Renderer *renderer, AppData *data_ptr);
@@ -88,10 +94,11 @@ void initialize(SDL_Renderer *renderer, AppData *data_ptr)
     //data_ptr->penguin_rect.h = 200;
 
     struct stat info;
-    int err = stat(std::string("/home").c_str(), &info);
+    std::cout << getenv("HOME") << std::endl;
+    int err = stat(getenv("HOME"), &info);
     if (err == 0 && S_ISDIR(info.st_mode))
     {
-        DIR* dir = opendir(std::string("/home").c_str());
+        DIR* dir = opendir(getenv("HOME"));
         // TODO: modify to be able to print all entries in directory in alphabetical order
         //       in addition to file name, also print file size (or 'directory' if entry is a folder)
         //       Example output:
@@ -112,20 +119,20 @@ void initialize(SDL_Renderer *renderer, AppData *data_ptr)
         struct stat file_info;
         for (int i = 0; i < data_ptr->files.size(); i++) {
             SDL_Color color = { 0, 0, 0 };
-            std::string full_path = "/home/" + data_ptr->files[i];
+            std::string full_path = getenv("HOME") + std::string("/") + data_ptr->files[i];
             file_err = stat(full_path.c_str(), &file_info);
             if (file_err) {
                 fprintf(stderr, "Uh oh, we shouldn't get here\n");
             }
             else if (S_ISDIR(file_info.st_mode)) {
                 printf("%s (directory)\n", data_ptr->files[i].c_str());
-                data_ptr->file_sizes.push_back("(directory)");
+                data_ptr->file_sizes.push_back(-1);
                 if (data_ptr->files[i] != ".") {
                 }
             }
             else {
                 printf("%s (%ld bytes)\n", data_ptr->files[i].c_str(), file_info.st_size);
-                data_ptr->file_sizes.push_back(std::string((char*)file_info.st_size));
+                data_ptr->file_sizes.push_back(file_info.st_size);
             }
             //file_err = stat(full_path.c_str(), &file_info);
             //std::string to_display = data_ptr->files[i] + std::string(" (") + std::string((char*)file_info.st_size) + std::string(")");
@@ -134,11 +141,22 @@ void initialize(SDL_Renderer *renderer, AppData *data_ptr)
             data_ptr->files_textures.push_back(file);
             SDL_FreeSurface(phrase_surf);
 
-            phrase_surf = TTF_RenderText_Solid(data_ptr->font, data_ptr->file_sizes[i].c_str(), color);
+            std::string file_size1 = std::to_string(data_ptr->file_sizes[i]) + std::string(" bytes");
+            phrase_surf = TTF_RenderText_Solid(data_ptr->font, file_size1.c_str(), color);
             SDL_Texture *file_size = SDL_CreateTextureFromSurface(renderer, phrase_surf);
             data_ptr->file_sizes_textures.push_back(file_size);
             SDL_FreeSurface(phrase_surf);
         }
+
+        SDL_Surface *img_surf = IMG_Load("resrc/images/folder_icon.png");
+        data_ptr->directory_icon = SDL_CreateTextureFromSurface(renderer, img_surf);
+        img_surf = IMG_Load("resrc/images/image_icon.png");
+        data_ptr->image_icon = SDL_CreateTextureFromSurface(renderer, img_surf);
+        img_surf = IMG_Load("resrc/images/right_arrow_icon.png");
+        data_ptr->right_arrow_icon = SDL_CreateTextureFromSurface(renderer, img_surf);
+        img_surf = IMG_Load("resrc/images/left_arrow_icon.png");
+        data_ptr->left_arrow_icon = SDL_CreateTextureFromSurface(renderer, img_surf);
+        SDL_FreeSurface(img_surf);
     }
 
     //SDL_Color color = { 0, 0, 0 };
@@ -155,21 +173,45 @@ void render(SDL_Renderer *renderer, AppData *data_ptr)
     
     // TODO: draw!
     //SDL_RenderCopy(renderer, data_ptr->penguin, NULL, &rect);
-    int curr_x = 10;
+    int curr_x = 60;
     int curr_y = 10;
 
-    for (int i = 0; i < data_ptr->files.size(); i++) {
+    int icon_y = 10;
+
+    for (int i = data_ptr->page * 15; i < data_ptr->page * 15 + 15; i++) {
         data_ptr->files_rect.push_back(new SDL_Rect);
+        data_ptr->icon_rect.push_back(new SDL_Rect);
+        data_ptr->icon_rect[i]->x = 10;
+        data_ptr->icon_rect[i]->y = icon_y;
+        data_ptr->icon_rect[i]->w = 40;
+        data_ptr->icon_rect[i]->h = 40;
         SDL_Rect rect = *data_ptr->files_rect[i];
         rect.x = curr_x;
         rect.y = curr_y;
+        SDL_RenderCopy(renderer, data_ptr->directory_icon, NULL, &(*data_ptr->icon_rect[i]));
         SDL_QueryTexture(data_ptr->files_textures[i], NULL, NULL, &(rect.w), &(rect.h));
         SDL_RenderCopy(renderer, data_ptr->files_textures[i], NULL, &rect);
-        rect.x = rect.x + 200;
-        SDL_QueryTexture(data_ptr->file_sizes_textures[i], NULL, NULL, &(rect.w), &(rect.h));
-        SDL_RenderCopy(renderer, data_ptr->file_sizes_textures[i], NULL, &rect);
-        curr_x;
+        rect.x = rect.x + 400;
+        if (data_ptr->file_sizes[i] != -1) {
+            SDL_QueryTexture(data_ptr->file_sizes_textures[i], NULL, NULL, &(rect.w), &(rect.h));
+            SDL_RenderCopy(renderer, data_ptr->file_sizes_textures[i], NULL, &rect);
+        }
+        //SDL_Surface *img_surf = IMG_Load("resrc/images/tux.png");
+        //data_ptr->penguin = SDL_CreateTextureFromSurface(renderer, img_surf);
+        //SDL_FreeSurface(img_surf);
         curr_y = curr_y + 30;
+        icon_y = icon_y + 30;
+
+        SDL_RenderCopy(renderer, data_ptr->left_arrow_icon, NULL, &(data_ptr->left_arrow_rect));
+        data_ptr->left_arrow_rect.x = 250;
+        data_ptr->left_arrow_rect.y = 500;
+        data_ptr->left_arrow_rect.w = 40;
+        data_ptr->left_arrow_rect.h = 40;
+        SDL_RenderCopy(renderer, data_ptr->right_arrow_icon, NULL, &(data_ptr->right_arrow_rect));
+        data_ptr->right_arrow_rect.x = 450;
+        data_ptr->right_arrow_rect.y = 500;
+        data_ptr->right_arrow_rect.w = 40;
+        data_ptr->right_arrow_rect.h = 40;
     }
     //SDL_QueryTexture(data_ptr->phrase, NULL, NULL, &(rect.w), &(rect.h));
     //rect.x = 10;
@@ -182,7 +224,7 @@ void render(SDL_Renderer *renderer, AppData *data_ptr)
 
 void quit(AppData *data_ptr)
 {
-    SDL_DestroyTexture(data_ptr->penguin);
+    SDL_DestroyTexture(data_ptr->directory_icon);
 
     //Destroy each file's texture
     int size = data_ptr->files_textures.size();
